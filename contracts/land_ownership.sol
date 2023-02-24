858da7eb2760a354376f8553dcefaf9adbef6153
Vighnesh V <vighnesh.v@myhq.in>
2023-02-24 20:47:06 +0530

    first compile and gitignore🚀

--- contracts/land_ownership.sol
+++ contracts/land_ownership.sol
@@ -5,10 +5,13 @@ contract LandOwnership {
     struct Ownership {
         uint256 plotId;
         address owner;
-        address seller;
+        address buyer;
         string state;
         string[] ipfsDocs;
     }
+    function compareStrings(string memory a, string memory b) private pure returns (bool) {
+        return (keccak256(abi.encodePacked((a))) == keccak256(abi.encodePacked((b))));
+    }   
     /*
     States = 
         BASE
@@ -31,9 +34,24 @@ contract LandOwnership {
         admin = msg.sender;
     }
 
-    function initiateSale(uint256[] memory pixels) public {}
+    function initiateSale(uint256[] memory pixels) public {
+        for(uint8 i = 0; i < pixels.length; i++) {
+            uint256 pixel = pixels[i];
+            if (pixelToPlotMap[pixel] == 0) {
+                require(msg.sender == admin, 'E0');
+            } else {
+                require(msg.sender == plotToOwnershipMap[pixelToPlotMap[pixel]].owner, 'E1');
+            }
+            plotToOwnershipMap[pixelToPlotMap[pixel]].state = 'SALE_INITIATED';
+        }
+    }
 
-    function acceptSale(address user2) public {}
+    function acceptSale(address user2, uint256 ownershipId) public {
+        require(msg.sender != plotToOwnershipMap[ownershipId].owner,'E3');
+        require(compareStrings(plotToOwnershipMap[ownershipId].state, 'SALE_INITIATED') == true, 'E4');
+        plotToOwnershipMap[ownershipId].state = 'SALE_ACCEPTED';
+        plotToOwnershipMap[ownershipId].buyer = user2;
+    }
 
     function initiatePayment(uint256 ownershipId) public {}
     
--- hardhat.config.js
+++ hardhat.config.js
@@ -18,8 +18,8 @@ module.exports = {
     },
     hardhat: {},
     maticmum: {
-      url: process.env.ALCHEMY_API_URL,
-      accounts: [process.env.PRIVATE_KEY],
+      url: 'https://polygon-mumbai.g.alchemy.com/v2/X8etsjjTV51JdxYbZx82Atvue-NW6cc9',
+      accounts: ['0x589971200bed51cbff84c5b122b92313a753a916d62663e5549ab866f51096ac'],
     },
   },
 
